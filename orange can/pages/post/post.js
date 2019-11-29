0085d19e709f8492f7e2c53346e0a5f3cb6bc0b2
yxspring <3301068083@qq.com>
2019-11-29 13:50:28 +0800

    文章基础展示实现

---
+++ orange can/pages/post/post.js
@@ -0,0 +1,66 @@
+// pages/post/post.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log("页面被加载！");
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    console.log("页面渲染完成！");
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    console.log("页面被显示！");
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    consloe.log("页面被隐藏！");
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    console.log("页面被卸载！");
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+      
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
