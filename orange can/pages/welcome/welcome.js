70e1a90a193161ea5f40f2d633c84973e615e36b
yxspring <3301068083@qq.com>
2019-11-30 15:09:48 +0800

    页面颜色修改,访问路径更改，学习了不同页面跳转方式的改变！

--- orange can/pages/welcome/welcome.js
+++ orange can/pages/welcome/welcome.js
@@ -1,3 +1,27 @@
 Page({
-  
+  data:{
+
+  },
+  onHide:function(){
+      console.log("pages onHide!");
+  },
+  onUnload:function(){
+    console.log("pages onUnload!");
+  },
+  onTapJump:function(event){
+    // 使用navigationTo使welcome页面 执行的是onhide 并不会onunload
+    wx.navigateTo({ 
+    // wx.redirectTo({  使用redirectTo页面会执行onUnload方法使的页面消除
+      url: '../post/post',
+      success: function () {
+        console.log("jump success !");
+      },
+      fail:function(){
+        console.log("jump fail !");
+      },
+      complete:function(){
+        console.log("jump complete!");
+      }
+    })
+  }
 })
